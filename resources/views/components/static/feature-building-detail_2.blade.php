86e6ac4f7a65ce827ce66d299ae2ddac2a32f2bb
Victor <vblanco1207@gmail.com>
2019-07-04 23:03:58 -0300

    Frontend fixes

--- resources/views/components/static/feature-building-detail_2.blade.php
+++ resources/views/components/static/feature-building-detail_2.blade.php
@@ -91,8 +91,93 @@
                 </div>
                 <!--/main slider carousel-->
             </div>
+        </div>
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-12">
+                    <div class="description-top">
+                        <h3 class="desc-title">Amenities</h3>
+                    </div>
+                    <div class="dept-details">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-12 box-icons">
+                                    <div class="row">
+
+                                        <div class="col-2 detail-box">
+                                            <img src="/img/icons/amen_03.png" alt="piscina">
+                                            <p style="text-transform: capitalize;">piscina</p>
+                                        </div>
+
+                                        <div class="col-2 detail-box">
+                                            <img src="/img/icons/amen_04.png" alt="piscina">
+                                            <p style="text-transform: capitalize;">sauna</p>
+                                        </div>
+
+                                        <div class="col-2 detail-box">
+                                            <img src="/img/icons/amen_02.png" alt="cochera">
+                                            <p style="text-transform: capitalize;">jacuzzi</p>
+                                        </div>
 
 
+                                        <div class="col-2 detail-box">
+                                            <img src="/img/icons/amen_05.png" alt="parrilla">
+                                            <p style="text-transform: capitalize;">parrilla</p>
+                                        </div>
+
+                                        <div class="col-2 detail-box">
+                                            <img src="/img/icons/amen_05.png" alt="parrilla">
+                                            <p style="text-transform: capitalize;">sum</p>
+                                        </div>
+
+                                        <div class="col-2 detail-box">
+                                            <img src="/img/icons/amen_05.png" alt="parrilla">
+                                            <p style="text-transform: capitalize;">solarium</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12">
+                <div class="description-top">
+                    <h3 class="desc-title">Contacto</h3>
+                </div>
+                    <div class="dept-contact">
+                        <form>
+                            <h3 class="contact-title">Comercializa: María</h3>
+                            <h3 class="contact-title-2">Cel. +549 1153294260</h3>
+                            <div class="contact-info">
+                                <div class="contact-info-item">
+                                    <input type="text" placeholder="Nombre">
+                                    <p class="contact-sub-info"></p>
+                                </div>
+                                <div class="contact-info-item">
+                                    <input type="text" placeholder="Teléfono">
+                                    <p class="contact-sub-info"></p>
+                                </div>
+                                <div class="contact-info-item">
+                                    <input type="email" placeholder="Email">
+                                    <p class="contact-sub-info"></p>
+                                </div>
+                                <div class="contact-info-item">
+                                    <textarea name="Mensaje" id="" cols="30" rows="10" placeholder="Mensaje"></textarea>
+                                    <p class="contact-sub-info"></p>
+                                </div>
+                            </div>
+                        </form>
+                        <div class="send-box">
+                            <div class="btn contact-send">
+                                <a href="#">Enviar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-md-6">
             <div class="dept-description mt-4">
                 <div class="description-top">
                     <h3 class="desc-title">Descripción</h3>
@@ -115,8 +200,29 @@
                     </p>
                 </div>
             </div>
+        </div>
 
-            {{-- <h3 class="info-title">Unidades</h3>
+        <div class="col-md-6">
+            <div class="dept-map mt-4">
+                <div class="map-top">
+                    <h3 class="map-title">Ubicación / Mapa</h3>
+                    <div class="map-socialmedia">
+                        <a href="#" target="_blank"><img src="/img/icons/whatsapp.svg" alt="Whatsapp"></a>
+                        <a href="#" target="_blank"><img src="/img/icons/facebook.svg" alt="Facebook"></a>
+                        <a href="#" target="_blank"><img src="/img/icons/instagram.svg" alt="Instagram"></a>
+                    </div>
+                </div>
+                <div class="map-bot">
+                    <div class="map-responsive">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3285.4543443022717!2d-58.48913148477123!3d-34.567368580468965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb6f55c9a4cbb%3A0x910191cd7ab1ace!2sGalv%C3%A1n+3051%2C+C1431FVD+CABA!5e0!3m2!1sen!2sar!4v1561140676743!5m2!1sen!2sar"
+                            width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <h3 class="info-title">Unidades</h3>
             <div class="dept-info">
                 <table class="table">
                     <thead>
@@ -194,110 +300,7 @@
                         </tr>
                     </tbody>
                 </table>
-            </div> --}}
-
-        </div>
-        <div class="col-md-6">
-            <div class="row">
-                <div class="col-12">
-                    <div class="dept-details">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-12 box-icons">
-                                    <div class="row">
-
-                                        <div class="col-2 detail-box">
-                                            <img src="/img/icons/amen_03.png" alt="piscina">
-                                            <p style="text-transform: capitalize;">piscina</p>
-                                        </div>
-
-                                        <div class="col-2 detail-box">
-                                            <img src="/img/icons/amen_04.png" alt="piscina">
-                                            <p style="text-transform: capitalize;">sauna</p>
-                                        </div>
-
-                                        <div class="col-2 detail-box">
-                                            <img src="/img/icons/amen_02.png" alt="cochera">
-                                            <p style="text-transform: capitalize;">jacuzzi</p>
-                                        </div>
-
-
-                                        <div class="col-2 detail-box">
-                                            <img src="/img/icons/amen_05.png" alt="parrilla">
-                                            <p style="text-transform: capitalize;">parrilla</p>
-                                        </div>
-
-                                        <div class="col-2 detail-box">
-                                            <img src="/img/icons/amen_05.png" alt="parrilla">
-                                            <p style="text-transform: capitalize;">sum</p>
-                                        </div>
-
-                                        <div class="col-2 detail-box">
-                                            <img src="/img/icons/amen_05.png" alt="parrilla">
-                                            <p style="text-transform: capitalize;">solarium</p>
-                                        </div>
-                                    </div>
-
-
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12">
-                    <div class="dept-contact">
-                        <form>
-                            <h3 class="contact-title">Comercializa: María</h3>
-                            <h3 class="contact-title-2">Cel. +549 1153294260</h3>
-                            <div class="contact-info">
-                                <div class="contact-info-item">
-                                    <input type="text" placeholder="Nombre">
-                                    <p class="contact-sub-info"></p>
-                                </div>
-                                <div class="contact-info-item">
-                                    <input type="text" placeholder="Teléfono">
-                                    <p class="contact-sub-info"></p>
-                                </div>
-                                <div class="contact-info-item">
-                                    <input type="email" placeholder="Email">
-                                    <p class="contact-sub-info"></p>
-                                </div>
-                                <div class="contact-info-item">
-                                    <textarea name="Mensaje" id="" cols="30" rows="10" placeholder="Mensaje"></textarea>
-                                    <p class="contact-sub-info"></p>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="btn contact-send">
-                        <a href="#">Enviar</a>
-                    </div>
-                </div>
             </div>
-            <div class="col-md-12">
-                <div class="dept-map mt-4">
-                    <div class="map-top">
-                        <h3 class="map-title">Ubicación / Mapa</h3>
-                        <div class="map-socialmedia">
-                            <a href="#" target="_blank"><img src="/img/icons/whatsapp.svg" alt="Whatsapp"></a>
-                            <a href="#" target="_blank"><img src="/img/icons/facebook.svg" alt="Facebook"></a>
-                            <a href="#" target="_blank"><img src="/img/icons/instagram.svg" alt="Instagram"></a>
-                        </div>
-                    </div>
-                    <div class="map-bot">
-                        <div class="map-responsive">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3285.4543443022717!2d-58.48913148477123!3d-34.567368580468965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb6f55c9a4cbb%3A0x910191cd7ab1ace!2sGalv%C3%A1n+3051%2C+C1431FVD+CABA!5e0!3m2!1sen!2sar!4v1561140676743!5m2!1sen!2sar" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-
-        </div>
-        <div class="col-md-8">
-
         </div>
     </div>
     </div>
