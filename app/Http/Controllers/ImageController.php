a04cba88eaf27f5f5a8abb061d289b6461c9946d
Tony Blanco <tony.blanco@softvision.com>
2019-07-29 18:47:13 -0300

    Working building workflow

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -13,33 +13,51 @@ class ImageController extends Controller
   public function upload(Request $request)
   {
     if($request->type == 'building'){
-      
       $property = Building::find($request->id);
+      $propertyImg = new BuildingImage();
+      $foreign = 'building_id';
     }else{
-      
       $property = Apartment::find($request->id);
+      $propertyImg = new ApartmentImage();
+      $foreign = 'apartment_id';
     }
-
+    
     if (count($request->images) > 0) {
-           
       
       // Create new directory
       \Storage::makeDirectory(config('images.properties_upload_path').$property->slug);
       $image_path = config('images.properties_upload_path').$property->slug;
-
-      collect($request->images)->each(function($img, $idx) use ($property, $request, $image_path) {
-
-        // Get extendsion and make filename
+      
+      collect($request->images)->each(function($img, $idx) use ($property, $request, $image_path, $propertyImg, $foreign) {
+        
+        // Get extension and make filename
         $ext= $img->getClientOriginalExtension();
         $filename = $request->type."_$idx".".".$ext;
-        \Image::make($img)
-              ->fit(config('images.properties_width'), config('images.properties_height'))
-              ->save("$image_path/$filename");
+        
+        // Check first if image is being used 
+        $imgUsed = $propertyImg->where([$foreign => $request->id, 
+                                       'filename'=> $filename ])
+                              ->select('filename')
+                              ->groupBy('filename')
+                              ->orderBy('filename', 'desc')
+                              ->first();
+        // If image already exists then extract image index: building_X.jpg
+        if($imgUsed) {
+          $idx = explode('_',$imgUsed->filename)[1][0] * 1;
+          $idx++;
+          $filename = $request->type."_$idx".".".$ext;
+        }
 
-        $img_input = ['filename'=> $filename, 
-                      'order'=> $idx,
-                    ];
-        $property->images()->create($img_input);
+        \Image::make($img)
+                ->fit(config('images.properties_width'), config('images.properties_height'))
+                ->save("$image_path/$filename");
+
+
+        $obj = $propertyImg->create([ $foreign=> $request->id, 
+                                      'filename'=> $filename,
+                                      'order'=> $idx]);
+
+        $property->images()->save($obj);
       });
     } 
     return response()->json(['msg'=> 'Success'], 201);
@@ -47,6 +65,15 @@ class ImageController extends Controller
   
   public function destroyApartmentImage($id) {
     $img = ApartmentImage::findOrFail($id);
+    $result = false;
+    if($img) {
+      $result = \Storage::delete($img->path) && $img->delete();
+    }
+    return response()->json(['msg'=> 'Delete', compact('result')], 202);
+  }
+
+  public function destroyBuildingImage($id) {
+    $img = BuildingImage::findOrFail($id);
     $result = false;
     if($img) {
       $result = \Storage::delete($img->path) && $img->delete();
