b66183eb1848262b76ffce71bca0282cea1582c6
nicolasov <nicolasolivavelez@gmail.com>
2019-08-21 11:19:28 -0300

    css

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -20,7 +20,7 @@
         <div class="row">
             <div class="col-md-12">
               <div class="appraisals-title">
-                <h2>{{ $config['title'] }}</h2>
+                <h2>{{ $config['title'] }}</h2><br>
               </div>
                <div class="appraisals-bottom text-center">
                 <p>{{ $config['top_text'] }}</p>
