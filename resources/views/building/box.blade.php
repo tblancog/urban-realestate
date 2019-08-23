42ee94a6e84d49fb4a9b7c97857455d469e08699
nicolasov <nicolasolivavelez@gmail.com>
2019-08-22 21:54:19 -0300

    Building box styling

--- resources/views/building/box.blade.php
+++ resources/views/building/box.blade.php
@@ -3,14 +3,14 @@
         <a href="{{ route('buildings.show', $building->slug) }}">
             <div class="box-img" style="background-image: url({{ count($building->images) > 0 ? $building->images->first()->path : '' }});">
                 <div class="box-info px-2 py-1">
-                    <div class="row col-12">
-                        <div class="col-6">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
                             <div class="status text-uppercase">@status_show($building->status)</div>
                             <div class="title">{{ $building->title }}</div>
                         </div>
-                        <div class="col-6">
-                            <div class="text-right finance text-uppercase">{{ 'Con financiación'}}</div>
-                            <div class="text-right price"> {{ $building->from_price ? 'desde  ' : '' }} USD
+                        <div class="col-md-6 col-sm-12">
+                            <div class="finance text-uppercase">{{ 'Con financiación'}}</div>
+                            <div class="price"> {{ $building->from_price ? 'desde  ' : '' }} USD
                                 @convert($building->price) </div>
                         </div>
                     </div>
