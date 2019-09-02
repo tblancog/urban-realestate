589aebab52d382bb75a6e6f3db6ab6f948d5bffd
Tony Blanco <tblanco82@gmail.com>
2019-09-01 22:34:37 -0300

    Under construction pages

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+
+        <title>Urban Real Estate</title>
+
+        <link rel="shortcut icon" href="assets/images/gt_favicon.png" />
+
+        <!-- Bootstrap itself -->
+        <link
+            href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css"
+            rel="stylesheet"
+            type="text/css"
+        />
+
+        <!-- Custom styles -->
+        <link rel="stylesheet" href="assets/css/magister.css" />
+
+        <!-- Fonts -->
+    </head>
+
+    <!-- use "theme-invert" class on bright backgrounds, also try "text-shadows" class -->
+    <body class="theme-invert">
+        <nav class="mainmenu">
+            <div class="container"></div>
+        </nav>
+
+        <!-- Main (Home) section -->
+        <section class="section" id="head">
+            <div class="container">
+                <div class="row">
+                    <div
+                        class="all col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1 text-center"
+                    >
+                        <a href=""
+                            ><img
+                                src="http://urbanrealestate.com.ar/img/core-img/logo.png"
+                                width="200"
+                        /></a>
+                        <!-- Site Title, your name, HELLO msg, etc. -->
+                        <h1 class="title">Sitio en mantenimiento</h1>
+                        <h2 class="subtitle">¡Estaremos de vuelta pronto!</h2>
+
+                        <!-- Short introductory (optional) -->
+                        <h3 class="tagline">
+                            Disculpe las molestias, pero estamos realizando
+                            algunas tareas de mantenimiento en este momento.
+                            <br />
+                            Si lo necesita, siempre puede contactarnos, de lo
+                            contrario, ¡volveremos a estar en línea en breve!
+                        </h3>
+
+                        <!-- Nice place to describe your site in a sentence or two -->
+                        <p>
+                            <a
+                                href="mailto:info@urbanrealestate.com.ar"
+                                class="btn btn-default btn-lg"
+                                >CONTACTANOS</a
+                            >
+                        </p>
+                    </div>
+                    <!-- /col -->
+                </div>
+                <!-- /row -->
+            </div>
+        </section>
+
+        <!-- Load js libs only when the page is loaded. -->
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+        <script src="assets/js/modernizr.custom.72241.js"></script>
+        <!-- Custom template scripts -->
+        <script src="assets/js/magister.js"></script>
+    </body>
+</html>
