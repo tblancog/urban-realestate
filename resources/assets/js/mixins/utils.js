089ebce23b4297a4cff11e406f73132c91fdbb33
Tony Blanco <tblanco82@gmail.com>
2019-09-01 21:36:30 -0300

    Fix route in image_path function in mixins

--- resources/assets/js/mixins/utils.js
+++ resources/assets/js/mixins/utils.js
@@ -1,67 +1,69 @@
-export default{
-data(){
-    return {
-        locations: [
-        'agronomia',
-        'almagro',
-        'balvanera',
-        'barracas',
-        'belgrano',
-        'boca',
-        'boedo',
-        'caballito',
-        'chacarita',
-        'coghlan',
-        'colegiales',
-        'constitucion',
-        'flores',
-        'floresta',
-        'liniers',
-        'mataderos',
-        'monserrat',
-        'monte castro',
-        'nueva pompeya',
-        'nuñez',
-        'palermo',
-        'parque avellaneda',
-        'parque chacabuco',
-        'parque chas',
-        'parque patricios',
-        'paternal',
-        'puerto madero',
-        'recoleta',
-        'retiro',
-        'saavedra',
-        'san cristobal',
-        'san nicolas',
-        'san telmo',
-        'velez sarsfield',
-        'versalles',
-        'villa crespo',
-        'villa del parque',
-        'villa devoto',
-        'villa gral. mitre',
-        'villa lugano',
-        'villa luro',
-        'villa ortuzar',
-        'villa pueyrredon',
-        'villa real',
-        'villa riachuelo',
-        'villa santa rita',
-        'villa soldati',
-        'villa urquiza',
+export default {
+    data() {
+        return {
+            locations: [
+                'agronomia',
+                'almagro',
+                'balvanera',
+                'barracas',
+                'belgrano',
+                'boca',
+                'boedo',
+                'caballito',
+                'chacarita',
+                'coghlan',
+                'colegiales',
+                'constitucion',
+                'flores',
+                'floresta',
+                'liniers',
+                'mataderos',
+                'monserrat',
+                'monte castro',
+                'nueva pompeya',
+                'nuñez',
+                'palermo',
+                'parque avellaneda',
+                'parque chacabuco',
+                'parque chas',
+                'parque patricios',
+                'paternal',
+                'puerto madero',
+                'recoleta',
+                'retiro',
+                'saavedra',
+                'san cristobal',
+                'san nicolas',
+                'san telmo',
+                'velez sarsfield',
+                'versalles',
+                'villa crespo',
+                'villa del parque',
+                'villa devoto',
+                'villa gral. mitre',
+                'villa lugano',
+                'villa luro',
+                'villa ortuzar',
+                'villa pueyrredon',
+                'villa real',
+                'villa riachuelo',
+                'villa santa rita',
+                'villa soldati',
+                'villa urquiza',
 
-    ]
-    }
-},
-  methods: {
-     image_path(item, index= 0, property= 'buildings') {
-      if (!item.hasOwnProperty('images') || item.images.length === 0) {
-          return 'img/core-img/placeholder.svg'
+            ]
         }
-          return 'uploads/properties/' + property + '/'
-                                       + item.id + '/'
-                                       + item.images[index].filename
-     }
-  }
+    },
+    methods: {
+        image_path(item, index = 0, property = 'buildings') {
+            if (!item.hasOwnProperty('images') || item.images.length === 0) {
+                return 'img/core-img/placeholder.svg'
+            }
+            let id = ''
+            if (property == 'buildings') {
+                id = item.id + '/'
+            }
+            return 'uploads/properties/' + property + '/' + id + item.images[index].filename
+        }
+    }
 }
