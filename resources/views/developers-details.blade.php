e88c4ac578a7cdcfa8caf618bd94537a0a51510d
nicolasov <nicolasolivavelez@gmail.com>
2019-09-05 23:51:37 -0300

    detail houses developers: html y css

--- resources/views/developers-details.blade.php
+++ resources/views/developers-details.blade.php
@@ -14,54 +14,41 @@
     <div class="south-load"></div>
 </div>
 
+<div class="container-fluid contentDetailHd">
 
-<!-- ##### Content Area Start ##### -->
-<section>
-    <div class="row details-view">
-        <div class="col-md-7">
+    <div class="row first-row"> <!-- start row 1 -->
+    
+        <div class="col-md-6 col-sm-12">
             <img src="img/bg-img/feature1.jpg" alt="">
         </div>
-        <div class="col-md-5">
-            <div class="details-info">
-                <div class="details-name">
-                    <h6>Proyecto</h6>
-                    <h3>Torre quesevendarapido</h3>
-                </div>
-                <div class="details-place">
-                    <h6>Ubicación</h6>
-                    <h3>Palermo, Argentina</h3>
-                </div>
-                <div class="details-year">
-                    <h6>Año</h6>
-                    <h3>2017</h3>
-                </div>
-                <div class="details-description">
-                    <h6>Descripción</h6>
-                    <p>
-
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non mauris leo. Cras maximus
-                        efficitur purus, ac ultrices leo vulputate in. Nam quis ullamcorper enim. Sed feugiat lacus dui,
-                        at molestie massa tempor ut. Praesent vehicula nisi ac vestibulum scelerisque. Duis venenatis
-                        quam sit amet magna lobortis sollicitudin. Vestibulum molestie mauris at ipsum ultrices, et
-                        mattis ante fringilla. Aliquam at suscipit nunc, eu rutrum felis. Morbi accumsan posuere
-                        tristique. Donec at nibh ut arcu finibus pretium. Nunc eu est id justo consectetur consectetur
-                        suscipit vitae lorem. Vivamus vulputate venenatis ornare.
-
-                        Donec ac efficitur ligula. Aliquam tincidunt a orci a vestibulum. Ut tristique erat nec libero
-                        lobortis, varius rhoncus neque pharetra. Praesent convallis, enim tristique tincidunt cursus,
-                        justo sem vehicula lacus, ut eleifend nisl dolor in lectus. Maecenas et justo id neque faucibus
-                        commodo. Sed odio massa, tincidunt at euismod sit amet, tristique non nisi. Sed risus nibh,
-                        laoreet vitae purus id, sollicitudin ornare justo. Sed ornare elementum augue, lacinia varius
-                        mauris gravida nec. Nulla egestas sagittis erat. Aliquam quis orci rhoncus, porta urna quis,
-                        posuere orci. Integer posuere lorem et augue elementum, vitae varius sapien tristique. Donec
-                        viverra rutrum ex sed viverra.
-
-                        Cras eget tincidunt massa.</p>
-                </div>
+    
+        <div class="col-md-6 col-sm-12">
+            <div class="detailsHD">
+                <h6>Proyecto</h6>
+                <h3>Torre quesevendarapido</h3>
+            </div>
+            <div class="detailsHD">
+                <h6>Año</h6>
+                <h3>2017</h3>
+            </div>
+            <div class="detailsHD">
+                <h6>Ubicación</h6>
+                <h3>Palermo, Argentina</h3>
+            </div>
+            <div class="detailsHD">
+                <h6>Servicios</h6>
+                <h3>Proyecto y Dirección de Obra</h3>
+            </div>
+            <div class="detailsHD">
+                <h6>Descripción</h6>
+                <p>La edificación evidenció, claros signos del paso del tiempo y los sistemas constructivos empleados en su construcción la hacían incompatible con los estándares de calidad y confort impuestos por la actual forma de vida. El conjunto arquitectónico presentaba una distribución interior que no respondía a las nuevas necesidades, pues los niveles funcionan como pisos independientes, comunicados por medio de una escalera exterior que dificulta el carácter unitario del residencial. Por otro lado la vivienda presenta patologías derivadas del agua, ocasionadas por el precario sistema de protección del semisótano,
+                </p>
             </div>
         </div>
-    </div>
-</section>
+    
+      </div> <!-- end row 1 -->
+
+</div> <!-- end container-fluid -->
 
 <!-- ##### Footer Area Start ##### -->
 @include('partials.front-footer')
