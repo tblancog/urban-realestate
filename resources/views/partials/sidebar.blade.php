1d6fb8be9ff662ddcb7f8376f1c676c71ed1ed17
nicolasov <nicolasolivavelez@gmail.com>
2019-09-07 15:46:41 -0300

    menu bo, front houses y devevlpers

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -9,7 +9,7 @@
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
-        <div class="user-panel">
+        <div class="user-panel" style="border-bottom: none !important">
             <div class="mt-3 d-flex">
                 <div class="image">
                     <img src="{{ asset('./img/profile.png') }}" class="img-circle elevation-2" alt="User Image">
@@ -17,7 +17,7 @@
                 <div class="info">
                     <a href="#abi" class="d-block">
                         {{Auth::user()->name}}
-                        <p>{{ 'usuario' }}</p>
+                        <p style="margin:0">{{ 'usuario' }}</p>
                         {{-- <p>{{Auth::user()->type}}</p> --}}
                     </a>
                 </div>
@@ -42,8 +42,8 @@
 
 
         <!-- Sidebar Menu -->
-        <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <nav class="">
+            <ul class="nav nav-pills nav-sidebar flex-column navDashboard" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
 
@@ -68,6 +68,11 @@
                 </li>
 
                 @endcan
+
+                <li style="border-bottom: 1px solid #4f5962; margin: 10px 0 15px;"></li>
+
+                <li><h3 style="font-family: 'AvantGarde', sans-serif; color: #C2C7D0;font-size: 1.1rem; text-transform: uppercase;padding: 0 0 8px 0; margin: 0px 0 10px 0; padding: 0;" class="">Real Estate</h3></li>
+                
                 <li class="nav-item">
                     <router-link to="/sliderAdmin" class="nav-link">
                         <i class="nav-icon fa fa-images green"></i>
@@ -124,30 +129,35 @@
                         </p>
                     </router-link>
                 </li>
-                <li class="nav-item">
-                    <br>
-                    <h3 class="text-uppercase text-center">Developers</h3>
+
+                <li style="border-bottom: 1px solid #4f5962; margin: 10px 0 15px;"></li>
+
+                <li><h3 style="font-family: 'AvantGarde', sans-serif; color: #C2C7D0;font-size: 1.1rem; text-transform: uppercase;padding: 0 0 8px 0; margin: 0px 0 10px 0; padding: 0;" class="">Developers</h3></li>
+                
+                <li class="nav-item">                    
                     <router-link to="/projectsAdmin" class="nav-link">
                         <i class="nav-icon fa fa-home green"></i>
-                        <p>
-                            Proyectos
-                        </p>
+                        <p>Proyectos</p>
                     </router-link>
                 </li>
 
-                  <li class="nav-item">
-                    <br>
-                    <h3 class="text-uppercase text-center">Houses</h3>
+                <li style="border-bottom: 1px solid #4f5962; margin: 10px 0 15px;"></li>
+
+                <li><h3 style="font-family: 'AvantGarde', sans-serif; color: #C2C7D0;font-size: 1.1rem; text-transform: uppercase;padding: 0 0 8px 0; margin: 0px 0 10px 0; padding: 0;" class="">Houses</h3></li>
+                
+                <li class="nav-item">
                     <router-link to="/housesAdmin" class="nav-link">
                         <i class="nav-icon fa fa-home green"></i>
-                        <p>
-                            Casas
-                        </p>
+                        <p>Casas</p>
                     </router-link>
                 </li>
+
+
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
+
+
     </div>
     <!-- /.sidebar -->
 </aside>
--- resources/views/partials/upper_navbar.blade.php
+++ resources/views/partials/upper_navbar.blade.php
@@ -4,7 +4,9 @@
     <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
+<!--        
         <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+-->  
       </li>
 
     </ul>
