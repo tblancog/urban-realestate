bdb0c6dbdf9fec181d42f95014334f13f0e291ce
Tony Blanco <tblanco82@gmail.com>
2019-10-14 21:59:27 -0300

    * Added cors policy * Set routes and assets urls to be relative

--- config/app.php
+++ config/app.php
@@ -150,17 +150,17 @@ return [
         /*
          * Package Service Providers...
          */
-
         /*
-         * Application Service Providers...
-         */
+        * Application Service Providers...
+        */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Intervention\Image\ImageServiceProvider::class
+        Intervention\Image\ImageServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
 
     ],
 
