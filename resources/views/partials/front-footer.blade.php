1bd900652f527caba0c135f5436edafa9084f35a
Tony Blanco <tblanco82@gmail.com>
2019-10-19 12:12:53 -0300

    Different phone and email address for

--- resources/views/partials/front-footer.blade.php
+++ resources/views/partials/front-footer.blade.php
@@ -11,27 +11,7 @@
                             </a>
                         </div>
                     </div>
-                    <div class="col-lg-4">
-                        <div class="text-center">
-                            <a href="https://wa.me/5491153294260" target="_blank">
-                                <img class="footer-icons" src="{{ asset('./img/core-img/icFooter2.png') }}"
-                                    alt="Developers">
-                                    &nbsp;+5411-5329-4260
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="text-center">
-                            <a href="{{ route('contact.index') }}">
-                                <img class="footer-icons" src="{{ asset('./img/core-img/icFooter3.png') }}"
-                                    >&nbsp;mep.urban@gmail.com
-                            </a>
-                            @if( in_array(request()->segment(1), ['developers', 'houses']) )
-                            <a href="https://www.facebook.com/urbanrealestate.ar/" target="_blank"><img width="50px;" src="{{ asset('/img/icons/icFacebook.png') }}" alt="Facebook"></a>
-                            <a href="http://instagram.com/urbanrealestate.ar" target="_blank"><img width="50px;" src="{{ asset('/img/icons/icInstagram.png') }}" alt="Instagram"></a>
-                            @endif
-                        </div>
-                    </div>
+                    @include('partials.phone-mail')
                 </div>
             </div>
         </div>
---
+++ resources/views/partials/phone-mail.blade.php
@@ -0,0 +1,64 @@
+@if(request()->segment(1) === 'developers')
+    <div class="col-lg-4">
+        <div class="text-center">
+            <a href="https://wa.me/5491145234418" target="_blank">
+                <img class="footer-icons" src="{{ asset('./img/core-img/icFooter2.png') }}" alt="Developers">
+                &nbsp;
+                011-4523-4418
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="text-center">
+            <a href="{{ route('contact.index') }}">
+                <img class="footer-icons"
+                    src="{{ asset('./img/core-img/icFooter3.png') }}">&nbsp;info@urbandevelopers.com.ar
+            </a>
+            <a href="https://www.facebook.com/urbanrealestate.ar/" target="_blank"><img width="50px;"
+                    src="{{ asset('/img/icons/icFacebook.png') }}" alt="Facebook"></a>
+            <a href="http://instagram.com/urbanrealestate.ar" target="_blank"><img width="50px;"
+                    src="{{ asset('/img/icons/icInstagram.png') }}" alt="Instagram"></a>
+        </div>
+    </div>
+
+@elseif(request()->segment(1) === 'houses')
+    <div class="col-lg-4">
+        <div class="text-center">
+            <a href="https://wa.me/5491153294260" target="_blank">
+                <img class="footer-icons" src="{{ asset('./img/core-img/icFooter2.png') }}" alt="Developers">
+                &nbsp;+5411-5329-4260
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="text-center">
+            <a href="{{ route('contact.index') }}">
+                <img class="footer-icons" src="{{ asset('./img/core-img/icFooter3.png') }}">&nbsp;info@urbanhouses.com.ar
+            </a>
+            <a href="https://www.facebook.com/urbanrealestate.ar/" target="_blank"><img width="50px;"
+                    src="{{ asset('/img/icons/icFacebook.png') }}" alt="Facebook"></a>
+            <a href="http://instagram.com/urbanrealestate.ar" target="_blank"><img width="50px;"
+                    src="{{ asset('/img/icons/icInstagram.png') }}" alt="Instagram"></a>
+        </div>
+    </div>
+@else
+    <div class="col-lg-4">
+        <div class="text-center">
+            <a href="https://wa.me/5491153294260" target="_blank">
+                <img class="footer-icons" src="{{ asset('./img/core-img/icFooter2.png') }}" alt="Developers">
+                &nbsp;+5411-5329-4260
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="text-center">
+            <a href="{{ route('contact.index') }}">
+                <img class="footer-icons" src="{{ asset('./img/core-img/icFooter3.png') }}">&nbsp;mep.urban@gmail.com
+            </a>
+            <a href="https://www.facebook.com/urbanrealestate.ar/" target="_blank"><img width="50px;"
+                    src="{{ asset('/img/icons/icFacebook.png') }}" alt="Facebook"></a>
+            <a href="http://instagram.com/urbanrealestate.ar" target="_blank"><img width="50px;"
+                    src="{{ asset('/img/icons/icInstagram.png') }}" alt="Instagram"></a>
+        </div>
+    </div>
+@endif
